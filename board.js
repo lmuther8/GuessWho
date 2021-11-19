4364305ecb2c5b6e2a0491b7ee0421cb5e949a20
lmuther8 <lmutes8@gmail.com>
2021-11-18 20:38:25 -0500

    dkjghsdkl

--- board.js
+++ board.js
@@ -10,7 +10,7 @@ function getBoard() {
     fetch(Url+'/board', {
 	method: 'get'
     })
-	.then (response => response.json() )
+	.then (response => response.text() )
         // .then (data => buildTable(data))
         .then (data => data)
 	.catch(error => {
