1eaac35cec5b36e3d1b36db7e5562256cd522d5a
lmuther8 <lmutes8@gmail.com>
2021-11-29 23:46:08 -0500

    Board Changes

--- public/board.js
+++ public/board.js
@@ -11,35 +11,33 @@ function getBoard() {
 	method:'get'
     })
 	.then (response => response.json() )
-        .then (data => buildTable(data))
+        .then (data => buildBoard(data))
 	.catch(error => {
 	    {alert("Error: Something went wrong:"+error);}
 	})
 }
 
-// Build output table from comma delimited list
-function buildTable(list) {
-  var table = '<table class="w3-table-all w3-hoverable" border="2"><tr><th>First Name</th><th>Last Name</th><th>URL</th>';
+function buildBoard(list) {
+  var board = '<div class="row">';
 
   for (var i = 0; i < list.length; i++) {
-table += "<tr><td>"+list[i]["First"]+"</td><td>"+list[i]["Last"]+"</td><td>"+list[i]["URL"]+"</td>";
+    board += '<div class="gamepiece col-sm-3">'+list[i]['First']+' '+list[i]['Last']+'<a><img src="'+list[i]['URL']+'"></a></div>';
   }
-  table += "</table>";
-  document.getElementById('gameBoard').innerHTML=table
+  board += "</div>";
+  document.getElementById('gameBoard').innerHTML=board
+
+  var pieces = document.querySelectorAll(".gamepiece");
+  console.log(pieces)
+  pieces.forEach(function(piece) {
+  	piece.addEventListener('click', function() {
+      if (piece.classList.contains('gamepiece-grey')) {
+        piece.classList.remove('gamepiece-grey');
+      }
+  		else {
+        piece.classList.add('gamepiece-grey');
+  	  }
+    })
+  })
+
   return null;
 }
-
-
-  //   var a = list.split(",");
-  //   if (a.length < 1) {
-	// return "<h3>Internal Error</h3>";
-  //   } else if (a.length == 1) {
-	// return "<h3>Nothing Found</h3>";
-  //   } else {
-	// var aLen = a.length;
-	// for (var i = 1; i < aLen; i+=5) {
-	//     result += "<tr><td class='professor'>"+a[i]+"</td><td class='professor'>"+a[i+1]+"</td><td class='professor'>"+a[i+2]+"</td><td class='type'>"+a[i+3]+"</td>";	}
-	// result += "</table>";
-  //
-	// return result;
-  //   }
