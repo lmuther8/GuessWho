60385250a582441e198259c4441d9ec5063560ab
Alana Winston-Khan <winstonkhan1@kenyon.edu>
2021-12-01 16:50:38 -0500

    picking a character

--- public/board.js
+++ public/board.js
@@ -1,9 +1,10 @@
 
 var port=9018;
 const Url='http://jimskon.com:'+port;
-
+var pickedChar = false;
 
 document.getElementById('board').addEventListener("click", getGameLayout);
+
 
 function getGameLayout(){
   getBoard()
@@ -23,6 +24,9 @@ function getBoard() {
 }
 
 function buildBoard(list) {
+  var pick = '<div class="row">Select a character for your opponent to guess:</div>';
+  document.getElementById('pickChar').innerHTML=pick;
+
   var board = '<div class="row">';
 
   for (var i = 0; i < list.length; i++) {
@@ -32,9 +36,24 @@ function buildBoard(list) {
   document.getElementById('gameBoard').innerHTML=board
 
   var pieces = document.querySelectorAll(".gamepiece");
+  //var hiddenChar;
   console.log(pieces)
   pieces.forEach(function(piece) {
   	piece.addEventListener('click', function() {
+
+      //pick character at start of game
+      if(!pickedChar){
+        //get name of character picked
+        //var hiddenChar= piece.innerHTML.substr(0, piece.innerHTML.indexOf(' '));
+        //var hiddenChar = piece.innerHTML;
+        //console.log(hiddenChar);
+        //console.log(piece);
+        console.log(piece.innerHTML);
+        displayhiddenChar(piece.innerHTML);
+        document.getElementById('pickChar').innerHTML='';
+        pickedChar=true;
+      }
+
       if (piece.classList.contains('gamepiece-grey')) {
         piece.classList.remove('gamepiece-grey');
       }
@@ -45,6 +64,11 @@ function buildBoard(list) {
   })
 
   return null;
+}
+
+function displayhiddenChar(hiddenChar) {
+  var hiddenChar = '<div class="gamepiece">'+hiddenChar+'</div>';
+  document.getElementById('hiddenChar').innerHTML=hiddenChar;
 }
 
 function buildGuessMenu() {
