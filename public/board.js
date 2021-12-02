446375b372507c0f52c56665d1c0699b725e3cd4
Alana Winston-Khan <winstonkhan1@kenyon.edu>
2021-12-01 19:45:17 -0500

    css updates

--- public/board.js
+++ public/board.js
@@ -65,7 +65,7 @@ function buildBoard(list) {
 
 function displayhiddenChar(hiddenChar) {
   //remove hover ability??
-  document.getElementById('hiddenChar').innerHTML='<div class="gamepiece center">'+hiddenChar+'</div>';
+  document.getElementById('hiddenChar').innerHTML='<div class="center">Opponent`s hidden character:</div><div class="gamepiece center">'+hiddenChar+'</div>';
 }
 
 function buildGuessMenu() {
