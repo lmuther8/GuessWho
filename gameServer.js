cac2b996b1ab4546c864deaee9ecad7edcb57027
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-02 21:40:18 -0500

    24

--- gameServer.js
+++ gameServer.js
@@ -52,7 +52,7 @@ function getRandomInt(min, max) {
 const databaseLength = 35;
 function buildIDList(length){
   var idList=[];
-  while (idList.length<21){
+  while (idList.length<24){
     var int = getRandomInt(1,databaseLength);
     if (!(idList.includes(int))){
       idList.push(int);
