ada90dd76586c2073129c29b7a46b76102d11e2b
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-03 12:21:30 -0500

    clean

--- gameServer.js
+++ gameServer.js
@@ -68,7 +68,6 @@ app.get('/board', function (req, res) {
     //get board pieces
     query = "SELECT * FROM Faculty WHERE ID in "+idList;
     // query = "SELECT * FROM Faculty WHERE ID in (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)";
-
     console.log(query)
     con.query(query, function(err,result,fields) {
 	     if (err) throw err;
