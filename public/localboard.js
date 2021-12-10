642c3cb51262c9992652071a76fe28933b84c8eb
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-09 19:41:12 -0500

    localgame

---
+++ public/localboard.js
@@ -0,0 +1,82 @@
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+const DATABASELENGTH = 34;
+function buildIDList(length){
+  var idList=[];
+  while (idList.length<length){
+    var int = getRandomInt(1,DATABASELENGTH);
+    if (!(idList.includes(int))){
+      idList.push(int);
+    }
+  }
+  idList="("+idList.join(",")+")"
+  return idList;
+}
+
+function getBoard(idlist) {
+  console.log('getting board')
+  fetch(Url+'/board/?find='+idlist, {
+	   method:'get'
+  })
+	.then (response => response.json())
+  .then (data => buildBoard(data))
+	.catch(error => {
+	    {alert("Error: Something went wrong:"+error);}
+	})
+}
+
+function buildBoard(list) {
+  console.log(list, list.length)
+  var pick = '<h6 class="row purple purple-text">Select a character for your opponent to guess:</h6>';
+  document.getElementById('pickChar').innerHTML=pick;
+
+  var board = '<div class="row board">';
+
+  for (var i = 0; i < list.length; i++) {
+    board += '<div class="gamepiece col-sm-3" style="margin-bottom: 0.8rem;text-align:center;" id="'+list[i]['First']+' '+list[i]['Last']+'">'+list[i]['First']+' '+list[i]['Last']+'<a><img src="'+list[i]['URL']+'"></a></div>';
+  }
+  board += "</div>";
+  document.getElementById('gameBoard').innerHTML=board
+
+  var pieces = document.querySelectorAll(".gamepiece");
+  console.log(pieces)
+  pieces.forEach(function(piece) {
+  	piece.addEventListener('click', function() {
+      if (piece.classList.contains('gamepiece-grey')) {
+        piece.classList.remove('gamepiece-grey');
+      }
+  		else {
+        piece.classList.add('gamepiece-grey');
+  	  }
+      //pick character at start of game
+      if(!pickedChar){
+        //get info about character picked
+        var mypick=[];
+        for (var i = 0; i < list.length; i++) {
+          if(list[i]['First']+' '+list[i]['Last']==piece.id){
+            mypick[0]=list[i]['First'];
+            mypick[1]=list[i]['Last']
+            mypick[2]=list[i]['URL'];
+          }
+        }
+
+        displayhiddenChar(mypick);
+        document.getElementById('pickChar').innerHTML='';
+        piece.classList.remove('gamepiece-grey');
+        pickedChar=true;
+        socket.emit('playerPicked',{name:myname, pick:piece.id});
+        buildGuessMenu();
+      }
+    })
+  })
+  return null;
+}
+
+function displayhiddenChar(hiddenChar) {
+  document.getElementById('hiddenChar').innerHTML='<h1>Your Mystery Character: '+mypick[0]+mypick[1]+"</h1>";
+}
