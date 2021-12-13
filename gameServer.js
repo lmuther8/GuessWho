8c56dfbc475a437b86f1ab9832094decb649f395
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-13 16:51:59 -0500

    fgh

--- gameServer.js
+++ gameServer.js
@@ -188,8 +188,9 @@ io.sockets.on('connection', function(socket) {
       console.log('gameStart');
       pickList=[];
       console.log(typeof(gameStart.room));
-      socket.broadcast.to(gameStart.room).emit('start', {query: gameStart.query});
-      socket.to(gameStart.room).emit('start', {query: gameStart.query});
+      // socket.broadcast.to(gameStart.room).emit('start', {query: gameStart.query});
+      // socket.to(gameStart.room).emit('start', {query: gameStart.query});
+      io.sockets.in(gameStart.room, 'start', {query: query});
     });
     socket.on('playerPicked', function(playerPicked) {
       console.log("playerpicked");
