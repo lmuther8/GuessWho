84d6f212bbae78f8896607373c5c7fa1e083628e
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-14 17:11:22 -0500

    true

--- public/chat.js
+++ public/chat.js
@@ -406,4 +406,4 @@ pieces.forEach(function(piece) {
 })
 };
 
-document.getElementById('guess-btn').addEventListener("click", makeGuessable, false);
+document.getElementById('guess-btn').addEventListener("click", makeGuessable, true);
