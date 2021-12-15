774a989f37249827e0e08098137b879fcc035a2c
Jack-Cohen <92878001+Jack-Cohen@users.noreply.github.com>
2021-12-15 12:51:33 -0500

    broadcast

--- gameServer.js
+++ gameServer.js
@@ -209,7 +209,8 @@ io.sockets.on('connection', function(socket) {
     });
     socket.on('localStart', function(localStart) {
       console.log('localStarted');
-      io.sockets.to(localStart.room).emit('localStartGame', {query: localStart.query});
+      socket.broadcast.to(localStart.room.emit('localStartGame',{query: localStart.query})
+      // io.sockets.to(localStart.room).emit('localStartGame', {query: localStart.query});
     });
 });
 
